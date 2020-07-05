56942acc1615841e6d141048cc49377dcda31423
Nick Stebb <nickolarse@riseup.net>
2020-07-06 10:32:51 +1200

    Update app.rb with fractal IVs

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
 require 'bundler/setup'
 require 'sinatra'
-require 'sinatra/reloader' if development?
+require 'sinatra/reloader'
 require 'tilt/erubis'
 require 'redcarpet'
 require 'yaml'
@@ -9,7 +9,7 @@ require 'uri'
 require_relative 'lib/habit'
 require_relative 'lib/objective'
 
-# require 'pry-remote'
+require 'pry'
 
 ROOT = File.expand_path('..', __FILE__)
 TEXT = YAML.load_file(ROOT + '/public/paragraphs_text.yaml')
@@ -344,7 +344,9 @@ get /\/habits\/fractal((\/(?:\d{1,})){1,})/ do
   habits_in_chain = params['captures'].first.split("/")[1..-1].map(&:to_i)
   @habits = habits_in_chain.map { |id| get_habit(id) }
   @habits.each { |h| h.update_to_today! }
-  @length_of_habit = @habits.first.length
+  @habits = @habits.sort_by! { |h| h.length }.reverse
+  @base_habit = @habits.first
+  @length_of_longest_habit = @base_habit.length
 
   @intro_spiel = render_markdown(params[:initial] ? TEXT[:existing_habit][:new] : TEXT[:existing_habit][:old])
   @page_title = "Fractal Habit Triangle"
@@ -359,6 +361,7 @@ post /\/habits\/fractal((\/(?:\d{1,})){1,})/ do
   @habit = @habits.first
   day_toggle_switch_value = ("completed-day-" + params[:node_completed_index])
   habit_node_for_day = @habit.get_nth(params[:node_completed_index].to_i)
+  binding.pry
   habit_node_for_day.today = params.key?(day_toggle_switch_value) ? 't' : 'f'
   redirect "/habits/fractal/#{@habit.id}"
 end
